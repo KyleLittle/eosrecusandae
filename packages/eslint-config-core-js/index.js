f895d4903bc61857643c627132acbc7e42e9e7bf
KyleLittle <shirleysimmonsf5d706951340@outlook.com>
2024-03-27 02:45:41 +0800

    fuga

---
+++ packages/eslint-config-core-js/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    es2017: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    semi: 'error',
+  },
+};
